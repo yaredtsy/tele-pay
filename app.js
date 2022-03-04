948e5e8c69a38dd46f76095755713eb6c4128455
yaredtsy <34584266+yaredtsy@users.noreply.github.com>
2022-03-03 22:14:02 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const https = require("https");
+const request = require("request");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/telepay/success/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    console.log(id);
+    const url = `https://us-central1-airtime-payment.cloudfunctions.net/payment/telebirr/paymentsuccess/${id}`;
+    console.log(url);
+
+    const body = await new Promise((resolve, reject) => {
+      https.get(url, (result) => {
+        resolve(result);
+        console.log(result.statusCode);
+        res.json({ 
+            success: "success" 
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+const start = () => {
+  try {
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
